93a62c94ae56a37582ed377c36950a93b7016dc5
Regan Sarwas <rsarwas@gmail.com>
2019-01-13 17:45:44 -0900

    Simplify count if n is prime
    Requires building a larger sieve (more time/memory), and a new is_prime() check (more time), but less time finding factors and counting.  Over all result is about 15% speedup.

--- src/euler/problem179.rs
+++ src/euler/problem179.rs
@@ -36,7 +36,7 @@ pub fn print_divisor_counts(n: usize) {
 ///
 /// Uses the prime sieve from the primal crate
 pub fn option1(n: usize) -> usize {
-    let sieve = primal::Sieve::new(isqrt(n));
+    let sieve = primal::Sieve::new(n);
     let mut count = 0;
     let mut prev = 0;
     for i in 2..=(n + 1) {
@@ -61,6 +61,9 @@ pub fn option1(n: usize) -> usize {
 /// Since we will call this many times, we have the caller generate a
 /// sieve of prime numbers that can be used to find the divisors
 pub fn divisor_count(n: usize, sieve: &primal::Sieve) -> usize {
+    if sieve.is_prime(n) {
+        return 2;
+    }
     let mut total = 1;
     // factor() returns a Result object; I'll panic if it is an Err()
     // It should only error if I don't give it enough primes, so it should be ok.
